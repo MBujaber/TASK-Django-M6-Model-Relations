3e661be55a631f23fe72b0ab2d8312a011a91f3c
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-10-18 19:16:41 +0300

    solved the tasks

--- classes/models.py
+++ classes/models.py
@@ -7,14 +7,15 @@ class Course(models.Model):
 
 class Lecture(models.Model):
     name = models.CharField(max_length=30)
+    course = models.ForeignKey("Course", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.name
-
+        return self.name, self.course
 
 class Slide(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
+    lecture = models.OneToOneField(Lecture, on_delete=models.CASCADE,primary_key=True,)
 
     def __str__(self):
         return self.name
@@ -23,6 +24,7 @@ class Slide(models.Model):
 class Assignment(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
+    lecture = models.OneToOneField(Lecture, on_delete=models.CASCADE, primary_key=True,)
 
     def __str__(self):
         return self.name
@@ -30,6 +32,7 @@ class Assignment(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course, related_name="tags")
 
     def __str__(self):
         return self.name
